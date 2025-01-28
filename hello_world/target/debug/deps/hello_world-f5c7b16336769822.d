313d318d2055b28011c35e109cf1f1b7ae6d748b
Yueen Ma <myueen@ad.unc.edu>
2025-01-28 01:52:31 +0000

    debug cargo build

---
+++ hello_world/target/debug/deps/hello_world-f5c7b16336769822.d
@@ -0,0 +1,5 @@
+/workspaces/comp423-rust/hello_world/target/debug/deps/hello_world-f5c7b16336769822: src/main.rs
+
+/workspaces/comp423-rust/hello_world/target/debug/deps/hello_world-f5c7b16336769822.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/workspaces/comp423-rust/hello_world/target/debug/hello_world: /workspaces/comp423-rust/hello_world/src/main.rs
